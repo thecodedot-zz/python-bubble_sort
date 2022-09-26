c1aa7e1567ff53887f8045d603ddda0347c8bfb0
thecodedot <72885755+thecodedot@users.noreply.github.com>
2022-09-26 16:35:36 +0500

    add iteration

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,7 +1,8 @@
-input_list = [10, 1, 2, 11, 25,34,56]
+input_list = [2, 10, 1, 25, 11, 56, 34]
 
 for i in range(len(input_list)):
 	for j in range(i):
 		if int(input_list[j]) > int(input_list[j+1]) :
 			input_list[j],input_list[j+1] = input_list[j+1],input_list[j]
-print(input_list)			
+	print(input_list)
+print('Bubble Sort', input_list)
